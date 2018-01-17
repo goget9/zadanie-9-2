04ee4e2078529320e438d27f46a491d405bd36cf
Woji <lubowicki.w@gmail.com>
2018-01-17 20:05:05 +0100

    Concat tables

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// scripts.js
+
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+
+var allNames = femaleNames.concat(maleNames);
+
+console.log(allNames);
+
+var newName = 'Marian';
+
+if (allNames.indexOf(newName) === -1) {
+
+	var x = allNames.push(newName);
+};
+
+console.log(allNames);
+
